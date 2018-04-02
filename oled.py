9e0ad5d06a2cad1f3bd170e51aa7b516eed4c69e
Jan Matecha <jan.matecha@gmail.com>
2018-04-02 13:00:26 +0200

    ovladac

--- oled.py
+++ oled.py
@@ -11,6 +11,9 @@ class Oled(SSD1306_I2C):
         self._height = 64
         self._head_width = self._width
         self._head_height = 10
+        self._foot_width = self._width
+        self._foot_height = 10
+
 
         super().__init__(128, 64, self.i2c)
         pass
@@ -30,10 +33,33 @@ class Oled(SSD1306_I2C):
         self.show()
 
     def square(self, x1=None, y1=None, x2=None, y2=None):
+        #TODO dodelat promenne pro ctverec
 
+        self.fill_rect(43, 11, 40, 40, 0)
         self.rect(43, 11, 40, 40, 1)
         self.show()
 
+    def show_position(self, x=None, y=None):
+        self.fill_rect(44, 12, 38, 38, 0)
+        self.pixel(x, y, 1)
+        self.line(x - 2, y, x + 2, y, 1)
+        self.line(x, y - 2, x, y + 2, 1)
+        self.show()
+        pass
+
+    def foot(self, width=None, height=None, ip=None):
+
+        if ip is None:
+            ip = get_ip()
+
+        if width is not None:
+            self.head_width = width
+        if height is not None:
+            self.head_height = height
+
+        self.fill_rect(0, 53, self.head_width, self.head_height, 0)
+        self.text(ip, 0, 55)
+        self.show()
 
     @property
     def head_width(self):
@@ -50,6 +76,24 @@ class Oled(SSD1306_I2C):
     @head_height.setter
     def head_height(self, value):
         self._head_height = value
+
+
+    @property
+    def foot_width(self):
+        return self._foot_width
+
+    @foot_width.setter
+    def foot_width(self, value):
+        self._foot_width = value
+
+    @property
+    def foot_height(self):
+        return self._foot_height
+
+    @foot_height.setter
+    def foot_height(self, value):
+        self._foot_height = value
+
 
     @property
     def width(self):
--- ovladac.py
+++ ovladac.py
@@ -1,6 +1,75 @@
+import ads1x15
+from machine import I2C, Pin
 from oled import *
+import socket  # for sockets
+import sys  # for exit
 
 oled = Oled()
 oled.head()
 oled.square()
 
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+ads = ads1x15.ADS1015(i2c=i2c)
+
+
+
+
+try:
+    # create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+    print('Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1])
+    sys.exit()
+
+print('Socket Created')
+
+#host = '127.0.0.1'
+host = '10.0.0.10'
+#host = '10.0.0.2'
+port = 8888
+
+
+# Connect to remote server
+s.connect((host, port))
+
+
+print('Socket Connected to ' + host )
+
+
+
+while True:
+    y = ads.read(0)
+    x = ads.read(1)
+    print(x,y)
+    x_new = int(x/42)+44
+    y_new = int(y/42)+12
+    print(x_new, y_new)
+    oled.show_position(x_new, y_new)
+
+# Send some data to remote server
+
+
+
+    message = "konec"
+    if y > 790:
+        message = "MF:" + str(int((y-790)/10))
+    if y < 790:
+        message = "MB:" + str(int((790-y)/10))
+
+
+    try:
+        # Set the whole string
+        s.sendall(message.encode('utf-8'))
+    except socket.error:
+        # Send failed
+        print('Send failed')
+        sys.exit()
+
+    print('Message send successfully')
+
+    # Now receive data
+    reply = s.recv(1024)
+
+    print(reply.decode('ascii'))
+
+s.close()
