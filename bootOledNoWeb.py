d724ffb9e8442ceb5ba16582c9af6941013c2b68
Jan Matecha <jan.matecha@gmail.com>
2018-04-06 06:30:32 +0200

    commit

---
+++ bootOledNoWeb.py
@@ -0,0 +1,33 @@
+from machine import I2C, Pin
+from ssd1306 import *
+
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+
+oled = SSD1306_I2C(128,64,i2c)
+
+print("start boot.py")
+oled.text("start boot.py",1,1,1)
+oled.show()
+
+#import myNetwork
+#text = myNetwork.do_connect()
+
+#oled.text(text[1][0],1,10,1)
+#oled.show()
+
+
+
+#import webrepl
+#oled.text("import webrepl",1,20,1)
+#oled.show()
+#webrepl.start()
+#oled.text("webrepl.start()",1,30,1)
+#oled.show()
+
+import gc
+gc.collect()
+
+print("konec boot.py")
+oled.text("konec boot.py",1,50,1)
+oled.show()
+
